2186cb1a209d34395e22d2047d6350f9f8dce140
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2022-09-20 14:55:35 +0000

    bookers2 3day

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          has_many :books, dependent: :destroy
+         
+         has_one_attached :ptofile_image
+         
+         
+         def get_profile_image(width, height)
+           unless profile_image.attached?
+             file_path = Rails.root.join('app/assets/images/no_image 2.jpeg')
+             ptofile_image.attach(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+           end
+           profile_image.variant(reize_to_limit: [width, height]).processed
+         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: "homes#top"
+  
   get '/home/about' => "homes#about"
+  
+  resources :books, only: [:new, :create, :index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
